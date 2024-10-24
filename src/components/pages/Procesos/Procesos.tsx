d84453951b7a18f1ce56ab48e8c498dfcfe7b7f7
dvillalba1 <dvillalba@excelsis.com.py>
2024-10-24 14:19:30 -0300

    Ajustado la seccion para que sea dinamica

---
+++ src/components/pages/Procesos/Procesos.tsx
@@ -0,0 +1,32 @@
+import { SectionProcesos } from "./components/SectionProcesos";
+
+export const Videos = () => {
+  return (
+    <>
+      <SectionProcesos
+        title="Servicios que ofrecemos"
+        subtitle="Resumen que podemos brindarte"
+        urlVideo="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        urlImagen="https://startup-template-sage.vercel.app/hero-light.png"
+        nombreVideo="Hero Video"
+        direccion="izquierda"
+      />
+      <SectionProcesos
+        title="Servicios que ofrecemos"
+        subtitle="Resumen que podemos brindarte"
+        urlVideo="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        urlImagen="https://startup-template-sage.vercel.app/hero-light.png"
+        nombreVideo="Hero Video"
+        direccion="derecha"
+      />
+      <SectionProcesos
+        title="Servicios que ofrecemos"
+        subtitle="Resumen que podemos brindarte"
+        urlVideo="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        urlImagen="https://startup-template-sage.vercel.app/hero-light.png"
+        nombreVideo="Hero Video"
+        direccion="izquierda"
+      />
+    </>
+  );
+};
