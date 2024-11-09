cabac261807eac51ba05113328515a75ef2f4842
tjara1 <tjara@excelsis.com.py>
2024-11-09 20:29:20 -0300

    Se agrega el uso de botones para mensajes y se cambia el texto del hero

---
+++ src/FloatingButton.tsx
@@ -0,0 +1,27 @@
+import { FaWhatsapp, FaEnvelope } from "react-icons/fa";
+
+const FloatingButton = () => {
+  return (
+    <div className="fixed z-50 flex flex-col space-y-3 bottom-4 right-4">
+      {/* Botón de WhatsApp */}
+      <a
+        href="https://wa.me/+595974853466"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-3 transition-all bg-[#3B505A] rounded-full shadow-lg hover:bg-[#9cf0ff]"
+      >
+        <FaWhatsapp size={30} color="white" />
+      </a>
+
+      {/* Botón de Mail */}
+      <a
+        href="mailto:admin@yvagacore.tech"
+        className="p-3 transition-all bg-[#3B505A] rounded-full shadow-lg hover:bg-[#9cf0ff]"
+      >
+        <FaEnvelope size={30} color="white" />
+      </a>
+    </div>
+  );
+};
+
+export default FloatingButton;
